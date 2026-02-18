860bc47ec31a17e2c5eddd342fac1f4ae1b00504
xeth hung <xethhung@xh-network.xyz>
2026-02-19 01:09:11 +0800

    update

--- digest-overall-price-info.py
+++ digest-overall-price-info.py
@@ -8,5 +8,8 @@ args = parser.parse_args()
 file_to_read=args.file
 data = json.loads(open(file_to_read, "r", encoding='utf-8').read())
 overall_data=json.dumps([{"company":data[key]['company'], "product": data[key]['product'], 'price': data[key]['price']} for key in data], indent=2, ensure_ascii=False)
+if len(overall_data) == 0:
+    raise ValueError(f"No data found: {open(file_to_read, 'r', encoding='utf-8').read()}")
+
 print(overall_data)
 
